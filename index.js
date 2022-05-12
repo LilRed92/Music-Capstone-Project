2518cb3b329f1733f57b59991db768725183cffa
Kacie Dearman <deardevdesigner@gmail.com>
2022-05-12 08:39:28 -0500

    Commit of what I've been working on. Sign up page does send user data to MongoDB. Need some work on pass security, css, requirements for the form, and some animation effects for pagination of the steps in the form.

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 4500
+const {MongoClient} = require('mongodb')
+const url = "mongodb+srv://kldearman:gQzAUYiB3LAb@musicwebsite.0ifll.mongodb.net/musicwebsite?retryWrites=true&w=majority"
+const client = new MongoClient(url)
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.post('/users', (req, res) => {
+
+	const docObj = {
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		dateOfBirth: req.body.dateOfBirth,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	}
+
+  async function insert() {
+	await client.connect()
+	const collection = client.db('test_db').collection('users')
+	await collection.insertOne(docObj)
+	await client.close()
+  }
+  insert()
+  res.redirect('/')
+})
+
+
+app.listen(port)
